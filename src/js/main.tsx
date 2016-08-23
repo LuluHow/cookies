4ba36ca6d608152f257b20a1aa6633e4047a0232
Ludovic Horem <ludo@l33t.home>
2016-08-23 11:03:14 +0200

    create branch dev

--- src/js/main.tsx
+++ src/js/main.tsx
@@ -10,12 +10,22 @@ export default class Main extends React.Component<IMainProps, {}> {
 	public constructor() {
 		super();
 
+		this.state = {
+			value: "Hello la W@C"
+		};
+
+	}
+
+	public handleClick(): void {
+		this.setState({
+			value: "GoodBye la W@C"
+		});
 	}
 
 	public render(): JSX.Element {
 		return (
 			<div>
-				<p>Hello la W@C</p>
+				<p onClick={ this.handleClick.bind(this) }>{ (this.state as any).value }</p>
 			</div>
 		)
 	}
