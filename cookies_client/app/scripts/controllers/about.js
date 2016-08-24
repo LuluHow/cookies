ffcb58b7e1fb355a3c1d9476c1f8aa3f2260a1d9
Ludovic Horem <ludo@L33T.local>
2016-08-24 09:39:28 +0200

    +angularJS

---
+++ cookies_client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cookiesClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cookiesClientApp
+ */
+angular.module('cookiesClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
